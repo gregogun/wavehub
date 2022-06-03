adcb5a20ad7d4908b53ae42928b235d2fe3c68f1
Greg Ogun <gregogun97@gmail.com>
2022-06-03 10:01:26 +0100

    feat: add player provider to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { Flex } from '@/ui/Flex';
 import { Nav } from '@/modules/layout/Nav';
 import { AppHeader } from '@/modules/layout/AppHeader';
 import { Box } from '@/ui/Box';
+import { PlayerProvider } from '@/modules/player/context';
 
 const GlobalStyle = globalCss({
   'html, body, #root, #__next': {
@@ -28,22 +29,24 @@ GlobalStyle();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <TooltipProvider>
-        <ThemeProvider
-          disableTransitionOnChange
-          attribute="class"
-          defaultTheme="dark"
-          value={{ light: 'light-theme', dark: darkTheme.toString() }}
-        >
-          <Flex css={{ height: '100%' }}>
-            <Nav />
-            <Box css={{ width: '100%' }}>
-              <AppHeader />
-              <Component {...pageProps} />
-            </Box>
-          </Flex>
-        </ThemeProvider>
-      </TooltipProvider>
+      <PlayerProvider>
+        <TooltipProvider>
+          <ThemeProvider
+            disableTransitionOnChange
+            attribute="class"
+            defaultTheme="dark"
+            value={{ light: 'light-theme', dark: darkTheme.toString() }}
+          >
+            <Flex css={{ height: '100%' }}>
+              <Nav />
+              <Box css={{ width: '100%' }}>
+                <AppHeader />
+                <Component {...pageProps} />
+              </Box>
+            </Flex>
+          </ThemeProvider>
+        </TooltipProvider>
+      </PlayerProvider>
     </AuthProvider>
   );
 }
